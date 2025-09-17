0b86c337e0f97d14cf7bba7fa9e72ef0454b2193
RogueElectron <anoosanoos12223@gmail.com>
2025-09-17 03:59:01 -0400

    added opaque-ts and made the base of the opaque logic

---
+++ back-end/logic/opaque-reg.py
@@ -0,0 +1,5 @@
+#register section
+#have to handle M variable
+# SecS, pub = the opaque create reg response function
+# pub is passed to the client
+#the user passes rec1 to the server
--- install-dep.sh
+++ install-dep.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt install libsodium-dev libopaque-dev libopaque0 
-print("dependencies installed, if you are not on debian, you might need to install libsodium bins")
+npm install @cloudflare/opaque-ts
+
+print("dependencies installed, if you are not on debian, you might need to install libsodium bins, on debian the headers and the binary are packaged under the same -dev package, i don't know what's the proccess on other systems")
