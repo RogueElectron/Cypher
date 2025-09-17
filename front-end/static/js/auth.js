3fcfcae19263b3be1400bf1b4074dd6db5ba04f3
RogueElectron <anoosanoos12223@gmail.com>
2025-09-17 05:55:04 -0400

    register

--- front-end/static/js/auth.js
+++
@@ -1,5 +0,0 @@
-import { OpaqueClient } from 'https://esm.sh/@cloudflare/opaque-ts' /*this is where things start to get, tricky*/
-
-// Initialize OPAQUE client
-const client = new OpaqueClient('P-256-SHA256')
-console.log('cdn method works') 
