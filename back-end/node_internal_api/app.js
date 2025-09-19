122b5805a2d3eccb5ff80cc5d5c69e429dbee73c
RogueElectron <anoosanoos12223@gmail.com>
2025-09-19 12:17:43 -0400

    making the python routing, and setting up node api routes

--- back-end/node_internal_api/app.js
+++ back-end/node_internal_api/app.js
@@ -1,16 +1,30 @@
+import { OpaqueServer } from '@cloudflare/opaque-ts';
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; // this might be changed, i believe it's best if we set an ENV for it
 
-// Middleware
+// so this converts json to js objects
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// registration routes
+app.get('/register/init', (req, res) => {
+ 
 });
 
-// Start server
+app.post('/register/finish', (req, res) => {
+  
+});
+
+// login routes
+app.get('/login/init', (req, res) => {
+  
+});
+
+app.post('/login/finish', (req, res) => {
+  
+});
+// to do, implement routing thru the python flask server
+// start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
