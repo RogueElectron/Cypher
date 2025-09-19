50d17c7c362c7389f82529ff67ddedc2022eed6e
RogueElectron <anoosanoos12223@gmail.com>
2025-09-19 12:17:43 -0400

    making the python routing, and setting up node api routes

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,16 @@
 from flask import Flask, send_from_directory, request, session, render_template, url_for, jsonify
 from flask_cors import CORS
 import base64
+from dotenv import load_dotenv
+import requests
+import os
+
+port = int(os.getenv('NODE_API_PORT', 3000))  # so it needs a default so i passed the one from the .env as default
+
+
+load_dotenv()
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)  # enable CORS for all routes
 
 
 #root
@@ -19,36 +27,42 @@ def serve_login():
     if request.method == 'GET':
         return render_template('login.html')
 
-# OPAQUE logic
-
-# step 1
-@app.route('/api/login/start', methods=['POST'])
-def handle_login_start():
-   return "" #placeholder
-#step 3
-@app.route('/api/login/finish', methods=['POST'])
-def handle_login_finish():
-   return "" #placeholder
-
 # register page
 @app.route('/api/register', methods=['GET'])
 def serve_register():
     if request.method == 'GET':
         return render_template('register.html')
 
-# OPAQUE logic
+# OPAQUE routing
+# so choom this just forwards it to the node js api
+# TODO IMPLEMENT INPUT SANITIZATION
 
-# step 1
-@app.route('/api/register/start', methods=['POST'])
-def handle_register_start():
-    # Print raw request data
+@app.route('/api/register/init', methods=['POST'])
+def handle_register_init():
     print(f"Headers: {dict(request.headers)}")
     print(f"Data: {request.get_data()}")
     return 'ok '
-#step 3
+
 @app.route('/api/register/finish', methods=['POST'])
 def handle_register_finish():
-   return "" #placeholder
+    url = 'http://127.0.0.1:3000/register/finish'
+    response = requests.post(url, json=request.json)
+    return response.content, response.status_code
+
+@app.route('/api/login/init', methods=['POST'])
+def handle_login_init():
+    url = 'http://127.0.0.1:3000/login/start'
+    response = requests.post(url, json=request.json)
+    return response.content, response.status_code
+
+@app.route('/api/login/finish', methods=['POST'])
+def handle_login_finish():
+    url = 'http://127.0.0.1:3000/login/finish'
+    response = requests.post(url, json=request.json)
+    return response.content, response.status_code
+
+
+
 
 # logout page
 @app.route('/api/logout', methods=['POST'])
