b8b63d6eab2c496de8c2280715c4b9e86e8336b3
RogueElectron <anoosanoos12223@gmail.com>
2025-09-22 16:04:34 -0400

    all thru to auth init works, i am, the one man brigade of digitopia

--- back-end/src/auth.js
+++ back-end/src/auth.js
@@ -14,40 +14,24 @@ document.addEventListener('DOMContentLoaded', async () => {
             event.preventDefault();
             const formData = new FormData(loginForm);
             const username = formData.get('username');
-            const password = formData.get('password');
+            const password = formData.get('password'); 
             const client = new OpaqueClient(config);
-            const ke1 = await client.authInit(password); // this function returns ke1 
-            const response = await fetch('/api/login/init', {
+            const ke1 = await client.authInit(password);  
+            const ke1Serialized = ke1.serialize();  
+            const ke1Base64 = btoa(String.fromCharCode(...ke1Serialized))
+            console.log(ke1);
+            
+            fetch('/api/login/init', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    ke1: ke1,
-                    username: username // if have enough time, make sure there's a
-                                       // consistent request format to all api endpoints
-                }),
-            });
-            // this returns ke2, the client must proccess it and arrive to the same 
-            // result as the server
-            const ke2 = await response.json();
-            //auth finish takes these [ke2, server_identity, client_identity]
-            // and returns ke3, session_key, export_key
-            // ke3 is sent to the server
-            // if the password is correct the client and server
-            // will arrive to the same session key
-            const authFinishResponse = await client.authFinish(ke2, serverIdentity, username);
-            console.log(authFinishResponse.sessionKey, 'session key');
-            fetch('/api/login/finish', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
                     username: username,
-                    ke3: authFinishResponse.ke3
-                }),
-            });    
+                    ke1Base64: ke1Base64
+                })
+            });
+            
             
         });
     }
